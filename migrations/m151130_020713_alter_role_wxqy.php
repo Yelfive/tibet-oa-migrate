74f1565c76cfbaf239201d09535b6d43ac36dd3b
windy <xfstudio@qq.com>
2015-11-30 15:45:47 +0800

    fix

--- migrations/m151130_020713_alter_role_wxqy.php
+++ migrations/m151130_020713_alter_role_wxqy.php
@@ -9,7 +9,7 @@ class m151130_020713_alter_role_wxqy extends Migration
      * Table name of which to be handled
      * @var string 
      */
-    public $table = '{{%table}}';
+    public $table = '{{%role}}';
     
     /**
      * Field name of which to be handled
@@ -20,18 +20,17 @@ class m151130_020713_alter_role_wxqy extends Migration
     public function up()
     {
         $sql = "ALTER TABLE $this->table (
-            ADD COLUMN 'wxid'  int NULL COMMENT '部门在微信企业号中的编号' AFTER 'after_id',
-            ADD COLUMN 'wxpid'  int NULL COMMENT '部门在微信企业号中的父编号' AFTER 'wxid'
+            ADD COLUMN wxid  int NULL COMMENT '部门在微信企业号中的编号' AFTER after_id,
+            ADD COLUMN wxpid  int NULL COMMENT '部门在微信企业号中的父编号' AFTER wxid
             ";
         $this->execute($sql);
     }
 
     public function down()
     {
-        $this->dropColumn($this->table,'user_card_number');
-        $this->dropColumn($this->table,'user_department_role');
+        $this->dropColumn($this->table,'wxid');
+        $this->dropColumn($this->table,'wxpid');
         echo "m151130_020713_alter_role_wxqy cannot be reverted.\n";
-        return false;
     }
     
     /*
